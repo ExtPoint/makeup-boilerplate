8d194f6ef12db6c7cc544c4d8d70230497e215b7
Kozhin Vladimir <affka@affka.ru>
2016-12-01 14:15:46 +0700

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # makeup-boilerplate
 Boilerplate for makeup landing and pages on html+less+webpack+react+bootstrap
+
+## Install
+
+### 1. Global replace - first important step
+
+Replace in every file of the project:
+
+- For human-readable labels: Boilerplate Makeup k4nuj8
+- For urls/db/other ids: boilerplate-makeup-k4nuj8 (including this file)
+- Rename the project in IDE
+
+### 2. Install dependencies
+
+```sh
+npm install
+```
+
+## Run for develop
+
+```
+npm start
+```
---
+++ webpack.js
@@ -0,0 +1,95 @@
+const fs = require('fs');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var CopyWebpackPlugin = require('copy-webpack-plugin');
+
+// Generate entries
+const entry = {
+    style: './frontend/less/index.less',
+};
+const templates = {};
+const imageDirs = [];
+function processDirEntries(name, parentName = null) {
+    ['js', 'less', 'html'].forEach(ext => {
+        if (fs.existsSync(`${__dirname}/frontend/${name}/index.${ext}`)) {
+            const key = name.replace(/\//g, '-');
+            if (ext === 'html') {
+                const path = `./${name}/index.${ext}`;
+                templates[key] = path;
+                if (parentName) {
+                    const parentKey = parentName.replace(/\//g, '-');
+                    if (!Array.isArray(templates[parentKey])) {
+                        templates[parentKey] = [];
+                    }
+                    templates[parentKey].push(path)
+                }
+            } else if (!entry[key]) {
+                entry[key] = `./frontend/${name}/index.${ext}`;
+            }
+        }
+    });
+
+    if (fs.existsSync(`${__dirname}/frontend/${name}/images`)) {
+        imageDirs.push({
+            from: `frontend/${name}/images`,
+            to: `images`,
+        });
+    }
+}
+fs.readdirSync(`${__dirname}/frontend`).forEach(name => {
+    if (name === 'less' || !fs.lstatSync(`${__dirname}/frontend/${name}`).isDirectory()) {
+        return;
+    }
+
+    fs.readdirSync(`${__dirname}/frontend/${name}`).forEach(subname => {
+        processDirEntries(`${name}/${subname}`, name);
+    });
+    processDirEntries(name);
+});
+
+const webpackEasy = require('webpack-easy');
+
+webpackEasy
+    .entry(entry)
+    .plugin([
+        imageDirs.length > 0 && new CopyWebpackPlugin(imageDirs),
+        !webpackEasy.isProduction() && new HtmlWebpackPlugin({
+            template: 'frontend/index.html',
+            filename: 'index.html',
+            TEMPLATE_NAMES: Object.keys(templates),
+            inject: false,
+        }),
+    ])
+    .plugin(Object.keys(templates).map(name => {
+        return new HtmlWebpackPlugin({
+            template: 'frontend/index.html',
+            filename: name + '.html',
+            inject: false,
+            BUNDLE_NAME: name,
+            FILES_TO_REQUIRE: [].concat(templates[name])
+        });
+    }))
+    .serverConfig({
+        contentBase: './web',
+        historyApiFallback: true,
+        proxy: {
+            '**': 'http://localhost',
+        },
+        staticOptions: {
+            '**': 'http://localhost',
+        },
+    })
+    .loader({
+        test: /\.(jpe?g|png|gif|svg)$/i,
+        loaders: [
+            'file?' + JSON.stringify({
+                hash: 'sha512',
+                digest: 'hex',
+                name: 'images/[hash].[ext]'
+            }),
+            'image-webpack?' + JSON.stringify({
+                bypassOnDebug: true,
+                optimizationLevel: 7,
+                interlaced: false
+            }),
+        ]
+    });
